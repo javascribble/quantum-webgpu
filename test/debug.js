d657c5deab8fa071c195824844bdf1a8619ea411
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-10-15 10:26:53 -0400

    merge repositories

--- test/debug.js
+++ test/debug.js
@@ -1,4 +1,4 @@
-import '/node_modules/@javascribble/quantum/source/main.js';
-import '/source/main.js';
+import '/node_modules/@javascribble/quantum/source/global.js';
+import '/source/global.js';
 
 document.body.style.visibility = 'visible';
