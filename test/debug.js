0be39dd1ff319b249553e87ef45eef3ce560fa55
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-02-22 08:53:27 -0500

    merge repositories

--- test/debug.js
+++ test/debug.js
@@ -1,4 +1,21 @@
 import '/node_modules/@javascribble/quantum/source/main.js';
 import '/source/main.js';
 
+const source = document.querySelector('img');
+
+canvas.addEventListener('resize', event => {
+    console.log(event);
+    canvas.drawImage({
+        source,
+        sx: 0,
+        sy: 0,
+        sw: source.width,
+        sh: source.height,
+        dx: 100,
+        dy: 100,
+        dw: source.width,
+        dh: source.height
+    });
+});
+
 document.body.style.visibility = 'visible';
