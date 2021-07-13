24b51d8a5250004c7b416fd2f4e188cd07cc0f8d
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-07-13 08:41:59 -0400

    update dependency

--- bundles/main-extensions.js
+++ bundles/main-extensions.js
@@ -1,3 +1,3 @@
-import '../source/extensions/math';
+import '../source/extensions/math.js';
 import '../source/extensions/renderer.js';
-import '../source/extensions/resources';
+import '../source/extensions/resources.js';
